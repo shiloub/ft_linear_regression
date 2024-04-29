c5caf404b8eccbba294bf59b177c8c878e0852c9
shiloub <achillemo@hotmail.fr>
2024-04-29 15:35:36 +0200

    message

--- training.py
+++ training.py
@@ -1,9 +1,19 @@
 from load_csv import load
 from matplotlib import pyplot as plt
-import time
 
 def estimatePrice(milleage, theta0, theta1):
     return theta0 + (milleage * theta1)
+
+
+def get_ab_math(datas) -> tuple:
+    x_bar = datas["km"].mean()
+    y_bar = datas["price"].mean()
+    m = len(datas)
+    cov_ab = sum(((datas["km"][i] - x_bar) * (datas["price"][i] - y_bar)) for i in range(m)) / m
+    a = cov_ab / datas["km"].var()
+    b = y_bar - (a * x_bar)
+    print(f"coef directeur calcule: {a}\nordonnee a l'origine calculee: {b}")
+    return(a, b)
 
 def gradient_descent(datas, theta0, theta1, learning_rate) -> tuple:
     m = len(datas)
@@ -16,11 +26,6 @@ def gradient_descent(datas, theta0, theta1, learning_rate) -> tuple:
         diff_sum2 = sum((estimatePrice(datas["km"][x], theta0, theta1) - datas["price"][x]) * datas["km"][x] for x in range(m))
         print(f"diff_sum = {diff_sum2}")
         test = 0
-        # for x in range(m):
-        #     # print(f"x = {x}")
-        #     temp = test + estimatePrice(datas["km"][x], theta0, theta1) - datas["price"][x]
-        #     # print(f"{test} + {estimatePrice(datas['km'][x], theta0, theta1)} - {datas['price'][x]} = {temp}")
-        #     test = temp
         theta0_temp =  theta0 - learning_rate * (1 / m) * diff_sum1
         theta1_temp =  theta1 - learning_rate * (1 / m) * diff_sum2
         theta0 = theta0_temp
@@ -32,14 +37,16 @@ def gradient_descent(datas, theta0, theta1, learning_rate) -> tuple:
 def main():
     cars = load("datas/data.csv")
     # cars = cars[:3]
-    print(cars)
-    a, b = gradient_descent(cars, 0, 0, 0.0001)
+    # print(cars)
+    # a, b = gradient_descent(cars, 0, 0, 0.0001)
+    a, b = get_ab_math(cars)
     plt.figure(figsize=(8, 6))
     plt.scatter(x=cars["km"],
                 y=cars["price"])
-    theta0 = a
-    theta1 = b
-    plt.plot(cars["km"], (cars["km"] * theta1) + theta0, color = "red", label="Regression lineaire")
+    theta0 = b
+    theta1 = a
+    plt.plot(cars["km"], (cars["km"] * theta1) + theta0, color = "red", label="Regression lineaire calculee")
+    plt.scatter(cars["km"].mean(), cars["price"].mean(), color = "red", label="G")
     plt.title("Nuage de points avec courbe de régression linéaire")
     plt.xlabel("Kilométrage")
     plt.ylabel("Prix")
